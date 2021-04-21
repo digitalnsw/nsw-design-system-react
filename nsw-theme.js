97d7676f2d120e32ebc078b7a4a675695277c4e9
Benjamin Chiong <benjamin.chiong@customerservice.nsw.gov.au>
2021-04-21 17:18:34 +1000

    Updates

--- nsw-theme.js
+++
@@ -1,9 +0,0 @@
-// .storybook/YourTheme.js
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'light',
-  brandTitle: 'NSW Design System React',
-  brandUrl: '/',
-  brandImage: 'https://www.digital.nsw.gov.au/sites/default/files/2021-04/nsw-ds-logo.png',
-});
