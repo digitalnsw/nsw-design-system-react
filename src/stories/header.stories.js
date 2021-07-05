9c07f4cb36731b8d9b9909366942b143ab4fe3c0
cdbenj <benjamin.chiong@customerservice.nsw.gov.au>
2021-07-05 15:47:43 +1000

    improve documentation

--- src/stories/header.stories.js
+++ src/stories/header.stories.js
@@ -5,7 +5,15 @@ import { SkipTo } from '../component/header/skipTo'
 
 export default {
   title: 'Globals/Header',
-  component: Header
+  component: Header,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Displays across the top of all NSW Government sites. For mobile menu to work, add the main navigation component'
+      }
+    }
+  }
 }
 
 const Template = (args) => (
@@ -16,25 +24,26 @@ const Template = (args) => (
   </>
 )
 
-export const WithoutSearch = Template.bind({})
-WithoutSearch.args = {
+export const Search = Template.bind({})
+Search.args = {
+  logo:
+    'https://www.digital.nsw.gov.au/themes/custom/corporateplus/nsw-digitalnsw-combined.svg',
+  headerUrl: '#',
+  department: 'digital.nsw',
   search: true,
+  mobile: true,
   onSubmit: (event) => {
     event.preventDefault()
     console.log(event.target.searchInput.value)
   },
-  mobile: true,
+}
+
+export const Simple = Template.bind({})
+Simple.args = {
   logo:
     'https://www.digital.nsw.gov.au/themes/custom/corporateplus/nsw-digitalnsw-combined.svg',
   headerUrl: '#',
-  department: 'digital.nsw'
-}
-
-export const WithoutMobileMenu = Template.bind({})
-WithoutMobileMenu.args = {
-  mobile: false,
   department: 'digital.nsw',
-  logo:
-    'https://www.digital.nsw.gov.au/themes/custom/corporateplus/nsw-digitalnsw-combined.svg',
-  headerUrl: '#'
+  mobile: false,
+  search: false
 }
