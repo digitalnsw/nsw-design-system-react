3cd4cfdc6dd75c17956f7f198d1421526f7a773b
TJ Harrop <tjharrop@gmail.com>
2021-11-27 12:05:39 +1100

    [component change] HeroBanner style selector (as = dark, light, white)

--- src/stories/heroBanner.stories.js
+++ src/stories/heroBanner.stories.js
@@ -8,47 +8,32 @@ export default {
   HeroBannerList,
 };
 
-const Template = (args) => (
-  <HeroBanner {...args} />
-);
-
-const BannerLinkList = (args) => (
-  <HeroBanner {...args}>
-    <HeroBannerList subtitle="Coronavirus links">
-      <ul>
-        <li><a href="/">COVIDSafe App</a></li>
-        <li><a href="/">Current COVID-19 case locations</a></li>
-        <li><a href="/">Travel to and from NSW</a></li>
-        <li><a href="/">Health and wellbeing</a></li>
-        <li><a href="/">Business and work</a></li>
-        <li><a href="/">COVID-19 testing centres</a></li>
-      </ul>
-    </HeroBannerList>
-  </HeroBanner>
-);
+const Template = (args) => <HeroBanner {...args} />
 
 export const Default = Template.bind({});
+export const Light = Template.bind({});
+export const Dark = Template.bind({});
 
 Default.args = {
   title: 'Helping you deliver great government services',
   intro: 'Find the building blocks for creating user-centred digital services, as well as policy, tools and guidance',
+  as: 'white',
   cta: { url: '/', text: 'Learn more' },
   image: { src: 'https://picsum.photos/id/237/2000/1250', alt: 'Labrador dog' },
 };
-
-export const Dark = Template.bind({});
 
 Dark.args = {
   title: 'Helping you deliver great government services',
-  dark: true,
+  as: 'dark',
   intro: 'Find the building blocks for creating user-centred digital services, as well as policy, tools and guidance',
   cta: { url: '/', text: 'Learn more' },
   image: { src: 'https://picsum.photos/id/237/2000/1250', alt: 'Labrador dog' },
 };
 
-export const LinkList = BannerLinkList.bind({});
-
-LinkList.args = {
+Light.args = {
   title: 'Helping you deliver great government services',
+  as: 'light',
   intro: 'Find the building blocks for creating user-centred digital services, as well as policy, tools and guidance',
+  cta: { url: '/', text: 'Learn more' },
+  image: { src: 'https://picsum.photos/id/237/2000/1250', alt: 'Labrador dog' },
 };
