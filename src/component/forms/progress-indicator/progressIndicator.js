f787f03c4cac576d2e2cb169b7fa158a0cd9855a
TJ Harrop <tjharrop@gmail.com>
2021-11-28 14:47:01 +1100

    Fixes #10

--- src/component/forms/progress-indicator/progressIndicator.js
+++ src/component/forms/progress-indicator/progressIndicator.js
@@ -9,8 +9,8 @@ export const ProgressIndicator = ({ step, of, ...attributeOptions }) => (
     <div className="nsw-progress-indicator__bar">
       {
           // eslint-disable-next-line react/no-array-index-key
-          [...Array(of)].map((value, index) => (index + 1 <= step ? <ProgressIndicatorStep key={index} active /> : <ProgressIndicatorStep key={index} />))
-    }
+          [...Array(of)].fill().map((value, index) => (index + 1 <= step ? <ProgressIndicatorStep key={index} active /> : <ProgressIndicatorStep key={index} />))
+        }
     </div>
   </div>
 );
